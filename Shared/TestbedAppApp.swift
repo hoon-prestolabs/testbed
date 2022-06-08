c9c079aa23bfd9d6d5176e911db60ce1526b726c
Hoon Hwangbo <hoon@prestolabs.io>
2022-06-08 17:55:53 +0900

    initial import

---
+++ Shared/TestbedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestbedAppApp.swift
+//  Shared
+//
+//  Created by Hoon Hwangbo on 2022/06/08.
+//
+
+import SwiftUI
+
+@main
+struct TestbedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
